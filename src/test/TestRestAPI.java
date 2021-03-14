535d0694edbf3e21bec18609a76b60f8de0a3406
bachuYoplait <ip9894@naver.com>
2021-03-15 00:02:09 +0900

    rest api 구현 (2021-03-16)

--- src/test/TestRestAPI.java
+++ src/test/TestRestAPI.java
@@ -1,5 +1,7 @@
 package test;
 
+import com.google.gson.Gson;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,16 +14,22 @@ public class TestRestAPI {
 
         System.out.println("접속 대기중~ ");
 
+
+        Gson gson = new Gson();
+
         while (true) {
             Socket sock = sSocket.accept(); // 새로운 소켓을 생성 클라이언트가 들어왔을때 , 접속했을때  실행되는 구문
             if (sock != null) {
+
                 System.out.println("사용자 접속 했습니다");
                 System.out.println("Client ip :" + sock.getInetAddress());
 
                 OutputStream ous = sock.getOutputStream();
                 DataOutputStream dous = new DataOutputStream(ous);
 
-                dous.writeInt(200);
+                //응답을 http 프로토골에 맞춰서 구현할 필요가있음
+                
+                dous.writeBytes(" \"status\": 200,");
                 dous.flush();
                 dous.close();
                 sock.close();
