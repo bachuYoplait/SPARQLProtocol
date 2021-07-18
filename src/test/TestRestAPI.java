deab9c09a49b76379f48aea6eb05a42a77a69394
bachuYoplait <foxcats1234@gmail.com>
2021-07-18 20:58:02 +0900

    Postman을 통한 소켓 통신. intputstream으로 header값 확인. outputstream으로 reponse 값 반환

--- src/test/TestRestAPI.java
+++ src/test/TestRestAPI.java
@@ -1,15 +1,17 @@
 package test;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 
 public class TestRestAPI {
 
     public static void main(String[] args) throws Exception {
-
         ServerSocket sSocket = new ServerSocket(1000);
 
         System.out.println("접속 대기중~ ");
@@ -20,18 +22,38 @@ public class TestRestAPI {
         while (true) {
             Socket sock = sSocket.accept(); // 새로운 소켓을 생성 클라이언트가 들어왔을때 , 접속했을때  실행되는 구문
             if (sock != null) {
+                byte[] byteArr = new byte[1000];
 
                 System.out.println("사용자 접속 했습니다");
                 System.out.println("Client ip :" + sock.getInetAddress());
 
+                InputStream is = sock.getInputStream();
+
+                //inputstream으로 request header를 확인
+                int readByteCount = is.read(byteArr);
+                String data = new String(byteArr, 0, readByteCount, "UTF-8");
+                System.out.println(data);
+
+
                 OutputStream ous = sock.getOutputStream();
                 DataOutputStream dous = new DataOutputStream(ous);
 
-                //응답을 http 프로토골에 맞춰서 구현할 필요가있음
-                
-                dous.writeBytes(" \"status\": 200,");
+                /**
+                HTTP/1.1 200 OK
+                Date: < some date >
+                Content-Type: application/json
+                Content-Length: < actual size >
+                {"example":"data"}
+
+                 프로토콜 규약에 맞춰서 DataOutputStream에 입력해줘야 Postman에서 정삭적으로 파싱이됨
+                 **/
+
+                dous.writeBytes("HTTP/1.1 200 OK\n" +
+                        "Content-Type: application/json\n" +
+                        "\n" +
+                        "{\"example\":\"data\"}");
                 dous.flush();
-                dous.close();
+                ous.close();
                 sock.close();
                 break;
             }
